e9df511b8e10d85820a75920861c7f01ec196a88
qwerty12794 <onzin09@gmail.com>
2016-11-03 10:12:23 +0100

    trying python

---
+++ GUI/Centrale/GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class GUI:
+    def __init__(self):
+        self.scherm = Tk()
+
+    def mainloop(self):
+        self.scherm.mainloop()
+
---
+++ GUI/INIT/__init__.py
@@ -0,0 +1,4 @@
+from GUI.Centrale import GUI
+
+gui = GUI.mainloop()
+
