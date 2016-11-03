183b2f16e7b3f4163d4f406b162998f9e05d78a8
plakband3 <joostholwerda@hotmail.com>
2016-11-03 11:17:02 +0100

    test.c voor arduino gereed gemaakt

--- C_Spul/test.c
+++ C_Spul/test.c
@@ -1,7 +1,11 @@
-void tempvalue() {       
- int analoogpoort=0;
- int waarde = analogRead(analoogpoort);
- Serial.write(1);
- delay(50);
- Serial.write(waarde);
+void loop() {
+  test();
 }
+
+ void test() {
+  int analoogpoort=0;
+  int waarde = analogRead(analoogpoort);
+  Serial.write(1);
+  delay(50);
+  Serial.write(waarde);
+ }
