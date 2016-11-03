43e764ba95b57b8cc4c6f504770753b81816f504
plakband3 <joostholwerda@hotmail.com>
2016-11-03 11:34:25 +0100

    temp file.

---
+++ C_Spul/temp.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(19200); //baud rate.
+}
+
+void loop() {
+  int var = Serial.read();
+  if (var == 1) {
+    test();
+  }
+}
+
+void temp() {
+  int analoogpoort=0;
+  int waarde = analogRead(analoogpoort);
+  Serial.write(1);
+  delay(50);
+  Serial.write(waarde);
+ }
