b2c533f171edf6d1aa286e758751230bb4fee98e
plakband3 <joostholwerda@hotmail.com>
2016-11-03 11:57:48 +0100

    temp, light, opjekt georient geprogrammeert.

--- C_Spul/temp.ino
+++ C_Spul/temp.ino
@@ -1,18 +1,40 @@
+int tempnr=0;
+int lightnr=2;
+
 void setup() {
   Serial.begin(19200); //baud rate.
 }
 
 void loop() {
   int var = Serial.read();
-  if (var == 1) {
-    temp();
+    
+  switch(var) {
+  case 1:
+  temp();
+  break;
+  case 2:
+  light();
+  break;
+  case 3:
+  Serial.write(9);
+  break;
   }
 }
 
-void temp() {
-  int analoogpoort=0;
-  int waarde = analogRead(analoogpoort);
+void light() {
+  int waarde = analogRead(lightnr);
   Serial.write(1);
   delay(50);
   Serial.write(waarde);
  }
+
+void temp() {
+  int waarde = analogRead(tempnr);
+  Serial.write(2);
+  delay(50);
+  Serial.write(waarde);
+}
+
+void lamp() {
+  
+}
